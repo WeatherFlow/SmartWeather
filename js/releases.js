8063e75c640baaa42af3814eb30e286ca507fc2a
Corrine Butcher <cbutcher@weatherflow.com>
2018-01-12 14:41:26 -0500

    Version selector

--- js/releases.js
+++ js/releases.js
@@ -2,4 +2,10 @@ $(document).ready(function () {
 	$("#app-menu").on("click, touchend", function () {
 		$("#sidebar").toggle();
 	});
+
+	$("#udp-versions").change(function () {
+		var version = $(this).val();
+		// https://weatherflow.github.io/SmartWeather/api/udp/v29/
+		window.location = "/SmartWeather/api/udp/" + version;
+	});
 })
