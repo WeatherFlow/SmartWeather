4d81631262c0c3251d14b88bd41cc3c2f95b0653
Corrine Butcher <cbutcher@weatherflow.com>
2019-12-18 15:51:58 -0500

    Adding debugging

--- js/code-challenge.js
+++ js/code-challenge.js
@@ -1,16 +1,19 @@
 $(document).ready(function () {
+    console.log("ready");
     attachEventHandlers();
 });
 
 function attachEventHandlers() {
-
+    console.log("attach event handlers");
     $("#generate-code-verifier").on("click touchend", function () {
+        console.log("generate code");
         $("#code-verifier").val(random_string(48));
 
         return false;
     });
 
     $("#code-verifier").on("change", function () {
+        console.log("on change");
         var verifier = $("#code-verifier").val();
         var challenge = base64_urlencode(sha256bin(verifier));
 
