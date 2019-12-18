cee9311faf7977c2a595fdeb3402a07596ab983e
Corrine Butcher <cbutcher@weatherflow.com>
2019-12-18 16:03:11 -0500

    Updating event handler

--- js/code-challenge.js
+++ js/code-challenge.js
@@ -12,7 +12,7 @@ function attachEventHandlers() {
         return false;
     });
 
-    $("#code-verifier").on("input", function () {
+    $("#code-verifier").on("input paste", function () {
         console.log("on change");
         var verifier = $("#code-verifier").val();
         var challenge = base64_urlencode(sha256bin(verifier));
