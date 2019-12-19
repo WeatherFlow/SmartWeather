4587b22a287d68f4b632af8fba33e350f786b260
Corrine Butcher <cbutcher@weatherflow.com>
2019-12-19 10:27:23 -0500

    Adding code verifier validation

--- js/code-challenge.js
+++ js/code-challenge.js
@@ -9,7 +9,6 @@ function attachEventHandlers() {
         $("#code-verifier").val(random_string(48));
         createCodeChallenge();
 
-
         return false;
     });
 
@@ -19,8 +18,17 @@ function attachEventHandlers() {
 }
 
 function createCodeChallenge() {
+    var isValid = true;
     var verifier = $("#code-verifier").val();
-    var challenge = base64_urlencode(sha256bin(verifier));
 
-    $("#code-challenge").html(challenge);
+    $("#code-challenge").html("");
+
+    if (verifier.length < 43 || verifier.length > 128) {
+        $("#error-msg").html("Invalid code verifier length.  Code verifier must be between 43 and 128 characters.");
+    } else if (/^[a-z0-9-._~]+$/i.test(verifier)) {
+        $("#error-msg").html("Code verifier contains invalid characters.");
+    } else {
+        var challenge = base64_urlencode(sha256bin(verifier));
+        $("#code-challenge").html(challenge);
+    }
 }
